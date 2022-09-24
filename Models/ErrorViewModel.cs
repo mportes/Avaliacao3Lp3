837f1ea643b24dd13f3e67ca9e35900e4270ee17
Matheus Portes <mportesif@gmail.com>
2022-09-24 20:29:58 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Avaliacao3BimLp3.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
